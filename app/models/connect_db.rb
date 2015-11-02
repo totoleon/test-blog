66214161aa9709f601803f0c8c92cf778f031e08
Li <hailol@a0999b0f809d.ant.amazon.com>
2015-11-02 16:54:41 +1100

    final

--- app/models/connect_db.rb
+++
@@ -1,4 +0,0 @@
-class ConnectDb < ActiveRecord::Base
-  establish_connection(:test)
-  self.table_name 'my_external_table'
-end
--- db/migrate/20151102050222_create_connect_dbs.rb
+++
@@ -1,8 +0,0 @@
-class CreateConnectDbs < ActiveRecord::Migration
-  def change
-    create_table :connect_dbs do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
